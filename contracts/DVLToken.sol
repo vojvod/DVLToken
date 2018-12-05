67d1d381c11fecb8f5072059c9f72af0ccaa99d4
vojvod <task@z40>
2018-12-05 11:07:46 +0200

    test

--- contracts/DVLToken.sol
+++ contracts/DVLToken.sol
@@ -2,7 +2,6 @@ pragma solidity >=0.4.22 <0.6.0;
 
 interface tokenRecipient { 
     function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; 
-    //simos
 }
 
 contract DVLToken {

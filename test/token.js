344e87ed156de4ba6c499caa8365ba9966a3fb0a
Simeon Taskaris <simos.taskaris@gmail.com>
2018-12-09 10:46:01 +0200

    test

--- test/token.js
+++ test/token.js
@@ -1,7 +1,14 @@
-contract('token', function(accounts) {
-  it("should assert true", function(done) {
-    var token = token.deployed();
-    assert.isTrue(true);
-    done();
+const DVLToken = artifacts.require("./DVLToken.sol");
+
+contract("DVLToken", accounts => {
+
+  it('should put 1000000000000 DVLToken in the first account', () => {
+    return DVLToken.deployed().then(function(instance) {
+      return instance.balanceOf.call(accounts[0]);
+    }).then(function(balance) {
+      console.log(balance);
+      assert.equal(balance.valueOf(), 1000000000000, "1000000000000 wasn't in the first account");
+    });
   });
-});
+
+})
